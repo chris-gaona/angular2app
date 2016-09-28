549f062c4fa8b5d95cd36a244b512ef3b8563b6d
Chris Gaona <thebeloved88@sbcglobal.net>
2016-09-28 15:49:07 -0700

    Gets data flowing properly on scoreboard page; completes tutorial project!

--- app/scoreboard.component.ts
+++ app/scoreboard.component.ts
@@ -10,14 +10,14 @@ import { ScoreboardItemComponent } from './scoreboardItem.component';
             <h1>Scoreboard</h1>
             <h3>Race Notifications</h3>
             <ul>
-                <li></li>   
+                <li *ngFor="let notification of notifications">{{notification}}</li>   
             </ul>
         </header>
         
         <div class="container-fluid scoreboard-display">
             <div class="row">
                 <div class="col-xs-4" *ngFor="let race of races">
-                    <scoreboard-item [race]="race"></scoreboard-item>
+                    <scoreboard-item [race]="race" (notification)="notifications.unshift($event)"></scoreboard-item>
                 </div>
             </div>
         </div>
@@ -27,6 +27,7 @@ import { ScoreboardItemComponent } from './scoreboardItem.component';
 
 export class ScoreboardComponent {
     races: Race[];
+    notifications: string[] = [];
 
     constructor(private raceService: RaceService) { }
 
--- app/scoreboardItem.component.ts
+++ app/scoreboardItem.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, Output, EventEmitter } from '@angular/core'
 import { Race } from './race'
 import { RaceScore } from './raceScore'
 import { RaceScoreService } from './raceScore.service'
@@ -6,26 +6,52 @@ import { RaceScoreService } from './raceScore.service'
 @Component({
     selector: 'scoreboard-item',
     template: `
-        <div class="scoreboard-item">
+        <div class="scoreboard-item" [class.finished]="score.currentLap >= score.totalLaps" *ngIf="score">
             <h2>{{race.name}}</h2>
-            <p></p>
+            <p>Lap {{score.currentLap}} of {{score.totalLaps}}</p>
             <ol>
-                <li>
-                    
+                <li *ngFor="let racer of score.racers">
+                    {{racer}}
                 </li>
             </ol>
         </div>
     `,
-    providers: [RaceScoreService]
+    providers: [RaceScoreService],
+    styles: [ `
+        .scoreboard-item {
+            border: 1px solid red;
+        }
+        
+        .scoreboard-item.finished {
+            border: 1px solid green;
+        }
+    `]
 })
 
 export class ScoreboardItemComponent {
     @Input() race: Race;
 
+    @Output() notification: EventEmitter<string> = new EventEmitter<string>();
+
+    score: RaceScore;
+
     constructor(private raceScoreService: RaceScoreService) { }
 
-    // ngOnInit() {
-    //     this.raceService.getRaces()
-    //         .subscribe(data => this.races = data);
-    // }
+    ngOnInit() {
+        this.raceScoreService.getScoreForRace(this.race.id)
+            .subscribe(data => {
+                this.checkForNotification(data)
+                this.score = data
+            });
+    }
+
+    checkForNotification(newScore) {
+        if (newScore.currentLap >= newScore.totalLaps) {
+            this.notification.emit(`The ${this.race.name} race has finished and ${newScore.racers[0]} is the winner!`);
+        }
+
+        if (this.score && newScore.racers[0] != this.score.racers[0]) {
+            this.notification.emit(`${newScore.racers[0]} has taken the lead in the ${this.race.name} race!`);
+        }
+    }
 }
